ffcae2080a4ec59d493c918f645f7db31535c3d3
Sehajdeep Singh <114660532+SehajDeepSingh26@users.noreply.github.com>
2023-09-17 19:57:55 +0530

    before Router. categories added

--- src/components/News.js
+++ src/components/News.js
@@ -79,7 +79,7 @@ export class News extends Component {
                 <div className="row">
                     {!this.state.loading && this.state.articles?.map((element) => {
                         return <div className='col-md-4' key={element.url}>
-                            <NewsItem title={element.title?element.title.slice(0,40):""} description={element.description?element.description.slice(0,60):""} imgURL={element.urlToImage} newsUrl={element.url}/>
+                            <NewsItem title={element.title?element.title.slice(0,50):""} description={element.description?element.description.slice(0,80):""} imgURL={element.urlToImage} newsUrl={element.url}/>
                         </div>
                     })}
 
